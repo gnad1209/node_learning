cb9ea31c55cd3b9520babfa9211afa891c9a199e
gnad1209 <danga120902@gmail.com>
2023-10-10 18:57:26 +0700

    backend

---
+++ back_end/src/config/db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+async function connect(){
+    try{
+    //    await mongoose.connect("mongodb://127.0.0.1:27017/f8_education_dev", {
+       await mongoose.connect("mongodb://127.0.0.1:27017/watchs", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+       console.log("kết nối thành côngggg")
+    }
+    catch(error){
+        console.log("kết nối ko thành công", error.message)
+    }
+}
+
+module.exports = { connect };
--- front_end/src/app/controller/SiteController.js
+++ front_end/src/app/controller/SiteController.js
@@ -5,21 +5,15 @@ const {mutipleMongooseToObject} = require('../../ulti/mongoose')
 
 class siteController {
     index(req, res,next) {
-        // try {
-        //     const courses = await Course.find({});
-        //     res.render('home',{ courses })
-        //     // res.json(course);
-        // }  catch (err) {
-        //     res.status(400).json({error: err});
-        // }
         SanPhams.find({})
             .then(sanphams =>{
-                // res.json(sanphams)
                 res.render('Home/Index',{ sanphams: mutipleMongooseToObject(sanphams) })
                 // res.render('demo',{ courses: mutipleMongooseToObject(courses) })
             })
             .catch(next)  
     }
+
+    
 
     search(req, res) {
         console.log(req.body);
